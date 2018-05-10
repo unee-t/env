4dbbc52f9d98434e78d1d1eecf9076bc304cf540
Kai Hendry <kai.hendry@unee-t.com>
2018-05-10 14:09:58 +0800

    Going by code

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/aws-sdk-go/aws/endpoints"
 )
 
 func main() {
@@ -29,36 +29,56 @@ func main() {
 
 	fmt.Println("STAGE:", stage)
 
-	domain, err := udomain(cfg, "foobar")
+	code, err := envcode(cfg)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	fmt.Println(domain)
+	fmt.Println(udomain(code, "whatever"))
 
 }
 
-func udomain(cfg aws.Config, service string) (string, error) {
+func envcode(cfg aws.Config) (int, error) {
 	svc := sts.New(cfg)
 	input := &sts.GetCallerIdentityInput{}
 
 	req := svc.GetCallerIdentityRequest(input)
 	result, err := req.Send()
 	if err != nil {
-		return "", err
+		return 0, err
 	}
 
 	log.Printf("Account: %v", result)
 
+	// https://github.com/unee-t/processInvitations/blob/master/sql/1_process_one_invitation_all_scenario_v3.0.sql#L16
 	switch accountID := aws.StringValue(result.Account); accountID {
-	case "192458993663":
-		return fmt.Sprintf("%s.unee-t.com", service), nil
-	case "915001051872":
-		return fmt.Sprintf("%s.demo.unee-t.com", service), nil
 	case "812644853088":
-		return fmt.Sprintf("%s.dev.unee-t.com", service), nil
+		return 1, nil
+	case "192458993663":
+		return 2, nil
+	case "915001051872":
+		return 3, nil
 	default:
-		return fmt.Sprintf("%s.dev.unee-t.com", service), nil
+		// Resort to staging if we don't recognise the account
+		log.Printf("Warning: Account ID %s is unknown, resorting to dev", accountID)
+		return 1, nil
+	}
+}
+
+func udomain(env int, service string) string {
+	if service == "" {
+		return ""
+	}
+	switch env {
+	case 1:
+		return fmt.Sprintf("%s.dev.unee-t.com", service)
+	case 2:
+		return fmt.Sprintf("%s.unee-t.com", service)
+	case 3:
+		return fmt.Sprintf("%s.demo.unee-t.com", service)
+	default:
+		log.Printf("Warning: Env %d is unknown, resorting to dev", env)
+		return fmt.Sprintf("%s.dev.unee-t.com", service)
 	}
 
 }
