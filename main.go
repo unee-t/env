2843dca9882f872376264d9bf1bac388023d99fe
Kai Hendry <kai.hendry@unee-t.com>
2018-05-10 15:08:50 +0800

    Rename to env

--- main.go
+++ main.go
@@ -1,84 +1,67 @@
-package main
+package env
 
 import (
 	"fmt"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
-type Env int
-
-var (
-	EnvUnknown Env = 0 // Oops
-	EnvDev     Env = 1 // Development aka Staging
-	EnvProd    Env = 2 // Production
-	EnvDemo    Env = 3 // Demo, which is like Production, for prospective customers to try
-)
-
-func main() {
-	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("uneet-dev"))
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	cfg.Region = endpoints.ApSoutheast1RegionID
-
-	// Assuming each account should have a STAGE variable set
-	stage, err := getSecret(cfg, "STAGE")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	fmt.Println("STAGE:", stage)
-
-	code, err := envcode(cfg)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Println(udomain(code, "whatever"))
-
+type Env struct {
+	Code EnvCode
+	cfg  aws.Config
 }
 
-func envcode(cfg aws.Config) (Env, error) {
+type EnvCode int
+
+// https://github.com/unee-t/processInvitations/blob/master/sql/1_process_one_invitation_all_scenario_v3.0.sql#L12-L16
+var (
+	EnvUnknown EnvCode = 0 // Oops
+	EnvDev     EnvCode = 1 // Development aka Staging
+	EnvProd    EnvCode = 2 // Production
+	EnvDemo    EnvCode = 3 // Demo, which is like Production, for prospective customers to try
+)
+
+func New(cfg aws.Config) (e Env, err error) {
+
+	// Save for ssm
+	e.cfg = cfg
+
 	svc := sts.New(cfg)
 	input := &sts.GetCallerIdentityInput{}
 
 	req := svc.GetCallerIdentityRequest(input)
 	result, err := req.Send()
 	if err != nil {
-		return 0, err
+		return e, err
 	}
 
 	log.Printf("Account: %v", result)
 
-	// https://github.com/unee-t/processInvitations/blob/master/sql/1_process_one_invitation_all_scenario_v3.0.sql#L16
 	switch accountID := aws.StringValue(result.Account); accountID {
 	case "812644853088":
-		return EnvDev, nil
+		e.Code = EnvDev
+		return e, nil
 	case "192458993663":
-		return EnvProd, nil
+		e.Code = EnvProd
+		return e, nil
 	case "915001051872":
-		return EnvDemo, nil
+		e.Code = EnvDemo
+		return e, nil
 	default:
 		// Resort to staging if we don't recognise the account
 		log.Printf("Warning: Account ID %s is unknown, resorting to dev", accountID)
-		return EnvDev, nil
+		return e, nil
 	}
 }
 
-func udomain(env Env, service string) string {
+func (e Env) udomain(service string) string {
 	if service == "" {
 		return ""
 	}
-	switch env {
+	switch e.Code {
 	case EnvDev:
 		return fmt.Sprintf("%s.dev.unee-t.com", service)
 	case EnvProd:
@@ -86,14 +69,14 @@ func udomain(env Env, service string) string {
 	case EnvDemo:
 		return fmt.Sprintf("%s.demo.unee-t.com", service)
 	default:
-		log.Printf("Warning: Env %d is unknown, resorting to dev", env)
+		log.Printf("Warning: Env %d is unknown, resorting to dev", e.Code)
 		return fmt.Sprintf("%s.dev.unee-t.com", service)
 	}
 
 }
 
-func getSecret(cfg aws.Config, store string) (string, error) {
-	ps := ssm.New(cfg)
+func (e Env) getSecret(store string) (string, error) {
+	ps := ssm.New(e.cfg)
 	in := &ssm.GetParameterInput{
 		Name:           aws.String(store),
 		WithDecryption: aws.Bool(true),
