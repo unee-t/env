7bac7dd33d8d73026a3bbab33022611d8e428771
Kai Hendry <hendry@webconverger.com>
2018-05-10 10:50:50 +0800

    Testing

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/awserr"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/ssmiface"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func main() {
+	cfg, err := external.LoadDefaultAWSConfig(external.WithSharedConfigProfile("uneet-dev"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	ssm := ssm.New(cfg)
+
+	fmt.Println("STAGE:", getSecret(ssm, "STAGE"))
+
+	svc := sts.New(cfg)
+	input := &sts.GetCallerIdentityInput{}
+
+	req := svc.GetCallerIdentityRequest(input)
+	result, err := req.Send()
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+
+}
+
+func getSecret(ssmapi ssmiface.SSMAPI, store string) string {
+	in := &ssm.GetParameterInput{
+		Name:           aws.String(store),
+		WithDecryption: aws.Bool(true),
+	}
+	req := ssmapi.GetParameterRequest(in)
+	out, err := req.Send()
+	if err != nil {
+		return ""
+	}
+	return aws.StringValue(out.Parameter.Value)
+}
