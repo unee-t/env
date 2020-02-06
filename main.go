3e74f76439e296b8ede7796d38e41ce83a0564d3
franck <franck@unee-t.com>
2020-02-06 11:15:16 +0800

    add a function `NewDbConnexion` to simplify how we generate string to connect to a database

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 
 	"github.com/apex/log"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/prometheus/client_golang/prometheus"
 	
 	"database/sql"
 
@@ -20,7 +22,15 @@ import (
 
 var pingPollingFreq = 5 * time.Second
 
-// environment is how we manage our differing {dev,demo,prod} AWS accounts
+//handlerSqlConnexion is a type of variabel to help us we manage our connexion to the SQL databases
+type handlerSqlConnexion struct {
+	DSN            string // aurora database connection string
+	APIAccessToken string
+	db             *sql.DB
+	environmentId  int
+}
+
+// environment is a type of variabel to help us we manage our differing {dev,demo,prod} AWS accounts
 type environment struct {
 	environmentId   int
 	Cfg       		aws.Config
@@ -134,70 +144,6 @@ func NewConfig(cfg aws.Config) (thisEnvironment environment, err error) {
 		}
 }
 
-func (thisEnvironment environment) Bucket(svc string) string {
-
-	// Most common bucket
-		if svc == "" {
-			svc = "media"
-		}
-
-	// We establish the ID of the Installation based on parameters INSTALLATION_ID
-	// This variable can be edited in the AWS parameter store
-		installationID := thisEnvironment.GetSecret("INSTALLATION_ID")
-
-	// If we have no installation ID we stop
-		if installationID == "" {
-			log.Fatal("Bucket fatal: installationID is unset, this is a fatal problem")
-		}
-
-	// To preserve legacy in case this is the Public Unee-T installation
-		if installationID == "main" {
-			// Preserve original bucket names
-			return fmt.Sprintf("%s-%s-unee-t", thisEnvironment.Stage, svc)
-		} else {
-			// Use INSTALLATION_ID to generate unique bucket name
-			return fmt.Sprintf("%s-%s-%s", thisEnvironment.Stage, svc, installationID)
-		}
-}
-
-func (thisEnvironment environment) SNS(name, region string) string {
-	// TODO: Check: if service name is empty, should this be a fatal error???
-	if name == "" {
-		log.Warn("SNS Warning: Service string empty")
-		return ""
-	}
-	return fmt.Sprintf("arn:aws:sns:%s:%s:%s", region, thisEnvironment.AccountID, name)
-}
-
-func (thisEnvironment environment) Udomain(service string) string {
-	if service == "" {
-		log.Warn("Udomain warning:Service string empty")
-		return ""
-	}
-
-	// We establish the domain for the Installation based on parameters DOMAIN
-	// This variable can be edited in the AWS parameter store
-		domain := thisEnvironment.GetSecret("DOMAIN")
-
-	// If we have no information on the domain then we stop
-		if domain == "" {
-			log.Fatal("Udomain fatal: DOMAIN is unset, this is a fatal problem")
-		}
-
-	// Based on the Environment we are in we do different things
-		switch thisEnvironment.environmentId {
-			case EnvDev:
-				return fmt.Sprintf("%s.dev.%s", service, domain)
-			case EnvProd:
-				return fmt.Sprintf("%s.%s", service, domain)
-			case EnvDemo:
-				return fmt.Sprintf("%s.demo.%s", service, domain)
-			default:
-				log.Fatal("Udomain fatal: Env is unknown, this is a fatal problem")
-				return ""
-		}
-}
-
 func (thisEnvironment environment) BugzillaDSN() string {
 
 	// Get the value of the variable BUGZILLA_DB_USER
@@ -284,6 +230,107 @@ func (thisEnvironment environment) BugzillaDSN() string {
 			bugzillaDbName)
 }
 
+// NewDbConnexion setups what we need to access the DB assuming various parameters have been setup in the AWS account
+
+func NewDbConnexion() (bzDbConnexion handlerSqlConnexion, err error) {
+
+	// We get the AWS configuration information for the default profile
+		cfg, err := external.LoadDefaultAWSConfig()
+		if err != nil {
+			log.WithError(err).Fatal("NewDbConnexion Fatal: We do not have the AWS credentials we need")
+			return
+		}
+
+	// We declare the environment we are in
+		thisEnvironment, err := NewConfig(cfg)
+		if err != nil {
+			log.WithError(err).Warn("NewDbConnexion Warning: error getting some of the parameters for that environment")
+		} else {
+			log.Infof("NewDbConnexion Log: This is the environment: %s", thisEnvironment.environmentId)
+		}
+
+	// cfg also needs the default region.
+	// We get the value for the DEFAULT_REGION
+		defaultRegion, ok := os.LookupEnv("DEFAULT_REGION")
+		if ok {
+			log.Infof("NewDbConnexion Log: DEFAULT_REGION was overridden by local env: %s", defaultRegion)
+		} else {
+			defaultRegion = thisEnvironment.GetSecret("DEFAULT_REGION")
+			log.Infof("NewDbConnexion Log: We get the DEFAULT_REGION from the AWS parameter store")
+		}
+	
+		if defaultRegion == "" {
+			log.Fatal("NewConfig fatal: DEFAULT_REGION is unset, this is a fatal problem")
+		}
+		
+		// Set the AWS Region that the service clients should use
+			cfg.Region = defaultRegion
+			log.Infof("NewDbConnexion Log: The AWS region for this environment has been set to: %s", cfg.Region)
+
+	// We get the value for the API_ACCESS_TOKEN
+		apiAccessToken, ok := os.LookupEnv("API_ACCESS_TOKEN")
+		if ok {
+			log.Infof("NewDbConnexion Log: API_ACCESS_TOKEN was overridden by local env: **hidden secret**")
+		} else {
+			apiAccessToken = thisEnvironment.GetSecret("API_ACCESS_TOKEN")
+			log.Infof("NewDbConnexion Log: We get the API_ACCESS_TOKEN from the AWS parameter store")
+		}
+	
+		if apiAccessToken == "" {
+			log.Fatal("NewConfig fatal: API_ACCESS_TOKEN is unset, this is a fatal problem")
+		}
+
+	// We have everything --> We create the database connexion string
+		bzDbConnexion = handlerSqlConnexion{
+			DSN:            thisEnvironment.BugzillaDSN(), // `BugzillaDSN` is a function that is defined in the uneet/env/main.go dependency.
+			APIAccessToken: apiAccessToken,
+			environmentId:  thisEnvironment.environmentId,
+		}
+	
+	// Test if this is working as it should
+		bzDbConnexion.db, err = sql.Open("mysql", bzDbConnexion.DSN)
+		if err != nil {
+			log.WithError(err).Fatal("NewDbConnexion fatal: error opening database")
+			return
+		} else {
+			log.Infof("NewDbConnexion Log: We can access the database")
+		}
+
+	microservicecheck := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "microservice",
+			Help: "Version with DB ping check",
+		},
+		[]string{
+			"commit",
+		},
+	)
+
+	// What is the version of this commit
+		version := os.Getenv("UP_COMMIT")
+
+	go func() {
+		for {
+			if bzDbConnexion.db.Ping() == nil {
+				microservicecheck.WithLabelValues(version).Set(1)
+				log.Infof("NewDbConnexion Log: Ping of the DB exited with 1")
+			} else {
+				microservicecheck.WithLabelValues(version).Set(0)				
+				log.Infof("NewDbConnexion Log: Ping of the DB exited with 0")
+			}
+			time.Sleep(pingPollingFreq)
+		}
+	}()
+
+	err = prometheus.Register(microservicecheck)
+	if err != nil {
+		log.Warn("NewDbConnexion Warning: prom already registered")
+	} else {
+		log.Infof("NewDbConnexion Log: prometheus registration is OK")
+	}
+	return
+}
+
 // Protect using: curl -H 'Authorization: Bearer secret' style
 // Modelled after https://github.com/apex/up-examples/blob/master/oss/golang-basic-auth/main.go#L16
 func Protect(h http.Handler, APIAccessToken string) http.Handler {
@@ -309,4 +356,68 @@ func Protect(h http.Handler, APIAccessToken string) http.Handler {
 // Wish I could make this simpler
 func Towr(h http.Handler) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) { h.ServeHTTP(w, r) }
+}
+
+func (thisEnvironment environment) Bucket(svc string) string {
+
+	// Most common bucket
+		if svc == "" {
+			svc = "media"
+		}
+
+	// We establish the ID of the Installation based on parameters INSTALLATION_ID
+	// This variable can be edited in the AWS parameter store
+		installationID := thisEnvironment.GetSecret("INSTALLATION_ID")
+
+	// If we have no installation ID we stop
+		if installationID == "" {
+			log.Fatal("Bucket fatal: installationID is unset, this is a fatal problem")
+		}
+
+	// To preserve legacy in case this is the Public Unee-T installation
+		if installationID == "main" {
+			// Preserve original bucket names
+			return fmt.Sprintf("%s-%s-unee-t", thisEnvironment.Stage, svc)
+		} else {
+			// Use INSTALLATION_ID to generate unique bucket name
+			return fmt.Sprintf("%s-%s-%s", thisEnvironment.Stage, svc, installationID)
+		}
+}
+
+func (thisEnvironment environment) SNS(name, region string) string {
+	// TODO: Check: if service name is empty, should this be a fatal error???
+	if name == "" {
+		log.Warn("SNS Warning: Service string empty")
+		return ""
+	}
+	return fmt.Sprintf("arn:aws:sns:%s:%s:%s", region, thisEnvironment.AccountID, name)
+}
+
+func (thisEnvironment environment) Udomain(service string) string {
+	if service == "" {
+		log.Warn("Udomain warning:Service string empty")
+		return ""
+	}
+
+	// We establish the domain for the Installation based on parameters DOMAIN
+	// This variable can be edited in the AWS parameter store
+		domain := thisEnvironment.GetSecret("DOMAIN")
+
+	// If we have no information on the domain then we stop
+		if domain == "" {
+			log.Fatal("Udomain fatal: DOMAIN is unset, this is a fatal problem")
+		}
+
+	// Based on the Environment we are in we do different things
+		switch thisEnvironment.environmentId {
+			case EnvDev:
+				return fmt.Sprintf("%s.dev.%s", service, domain)
+			case EnvProd:
+				return fmt.Sprintf("%s.%s", service, domain)
+			case EnvDemo:
+				return fmt.Sprintf("%s.demo.%s", service, domain)
+			default:
+				log.Fatal("Udomain fatal: Env is unknown, this is a fatal problem")
+				return ""
+		}
 }
